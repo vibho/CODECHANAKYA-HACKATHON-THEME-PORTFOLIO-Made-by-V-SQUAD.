d9e35f1859325160a4d856d282095a323a691da4
vibho <rmsvgupta8261@gmail.com>
2020-09-18 05:24:06 -0500

    First Commit

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-inverse navbar-fixed-top"> 
+    <div class="container"> 
+        <div class="navbar-header"> 
+            <button type="button" class="navbar-toggle btn-danger" data-toggle="collapse" data-target="#myNavbar"> 
+                <span class="icon-bar"></span> 
+                <span class="icon-bar"></span> 
+                <span class="icon-bar"></span> 
+            </button> <a class="navbar-brand" href="index.php">VAIBHAV GUPTA</a> 
+        </div> 
+        <div class="collapse navbar-collapse" id="myNavbar"> 
+            <ul class="nav navbar-nav navbar-right"> 
+                <li><a href="#skills"><span class="glyphicon glyphicon-user"></span> Skills</a></li>
+                <li><a href="#contact"><span class="glyphicon glyphicon-phone"></span> Contact </a></li> 
+                <li><a href="#about"><span class="glyphicon glyphicon-tasks"></span> About </a></li>
+                <li><a href="#projects"><span class="glyphicon glyphicon-book"></span> Projects </a></li>
+            </ul> 
+        </div> 
+    </div> 
+</div>
---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Vaibhav Gupta</title>
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        
+        
+        <!-- jQuery -->
+        <script src="js/jquery-3.5.1.min.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body style="padding-top: 0px;">
+        <!-- Header -->
+        <?php
+        include'includes/header.php';
+        ?>
+        <!--Header end-->
+        
+        <slider>
+            <slide><p><h1>H'i I'm Vaibhav Gupta</h1><h2>A computer science student passionate about programming and design.</h2></slide>
+            <slide><p>Be stronger than your excuses</p></slide>
+            <slide><p style="color: #737373">  IT'S THE WILL.   NOT THE SKILL.  </p></slide>
+            <slide><p style="color: #ffffff">A Winner Is Just A Loser Who Tried One More Time.</p></slide> 
+        </slider>
+
+    
+            <!--Main banner image end-->
+            
+            <div style="padding-top: 650px" id="about"><center><u><h1>About</h1></u></center></div>
+            <div class ="container">
+            <div class="row">
+                <div class="col-md-2 col-xs-3">                 
+                      <img src="./img/v.jpg" alt="" class="img-responsive img-circle">  
+                </div>
+                <div class="col-md-9 col-md-offset-1 col-xs-9">
+                    
+                        <p><h3>My Name is Vaibhav gupta.I'm from Prayagraj a B.Tech Student At UNITED COLLEGE OF ENGINEERING AND MANAGEMENT in the COMPUTER SCIENCE department. Which is affiliated From DR.APJ ABDUL KALAM UNIVERSITY.
+And I have completed my high school and Intermediate with an aggregate percentage.
+I'm inspired by some of the average Students but they well suceeded in life, My first inspiration comes from Ritesh Agarwal he is the Founder of Oyo rooms and the most important part is he was a college dropout, And there it comes my second most ideal as a computer science engineer
+Shantanu Narayen CEO of Adobe.
+I had a lot of interest in Coding because coding was in my veins since 4 years back, I aim to become a successful engineer and wanted to be placed in a respectful company like Infosys, Wipro, Amazon, etc.
+My hobbies are working out in the gym, loves to TRAVEL And want to explore the world, And passionate about GAMING stuff.
+And I have some of the skills I'm time punctual, and I have fine leadership quality.</h3></p> 
+                </div>
+            
+            </div>
+            </div>
+             
+            <div id="skills" "><center><u><h1>Skills</h1></u></center></div>
+            <!--Item categories listing-->
+            <div class="container">
+                <div class="row text-center" id="item_list">
+                    <div class="col-sm-4">
+                            <div class="thumbnail">
+                                <img src="img/12.jpg" alt="">
+                                <div class="caption">
+                                    <h3><u>C/C++</u></h3>
+                                    <h4><span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star-empty"></span>
+                                    </h4>
+                                </div>
+                            </div> 
+                    </div>
+
+                    <div class="col-sm-4">
+                            <div class="thumbnail">
+                                <img src="img/10.jpg" alt="">
+                                <div class="caption">
+                                    <h3><u>HTML/CSS</u></h3>
+                                    <h4><span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star-empty"></span>
+                                    </h4>
+                                </div>
+                            </div> 
+                    </div>
+
+                    <div class="col-sm-4">
+                            <div class="thumbnail">
+                                <img src="img/9.jpg" alt="">
+                                <div class="caption">
+                                    <h3><u>Java</u></h3>
+                                    <h4><span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star"></span>
+                                        <span class="glyphicon glyphicon-star-empty"></span>
+                                        <span class="glyphicon glyphicon-star-empty"></span>
+                                    </h4>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+            <div id="projects" "><center><u><h1>Projects</h1></u></center></div>
+            <!--Item categories listing-->
+            <div class="container">
+                <div class="row text-center" id="item_list">
+                    <div class="col-sm-4">
+                        <a href="https://lifestyleforale.000webhostapp.com/" >
+                            <div class="thumbnail">
+                                <img src="img/6.jpg" alt="">
+                                <div class="caption">
+                                    <h3>Web development</h3>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="https://github.com/vibho" >
+                            <div class="thumbnail">
+                                <img src="img/8.jpg" alt="">
+                                <div class="caption">
+                                    <h3>C/C++ Codes</h3>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+
+                    <div class="col-sm-4">
+                        <a href="https://www.hackerrank.com/VaibhavGupta2nd?hr_r=1" >
+                            <div class="thumbnail">
+                                <img src="img/hack.jpeg" alt="">
+                                <div class="caption">
+                                    <h3>Hackerrank</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!--Item categories listing end-->
+            <div id="#contact"><center><u><h1>Contact</h1></u></center></div>
+            <div style="margin: 50px 0 100px 0"><p><center><i>Feel free to get in touch.Cheers!</i></center></p></div>
+            <div><center><u><h2>E-Mail</h2></u></center></div>
+            <div style="margin: 40px 0 100px 0"><p><center><i>rmsvgupta8261@gmail.com</i></center></p></div>
+            <div><center><u><h2>Twitter</h2></u></center></div>
+            <div style="margin: 40px 0 100px 0"><p><center><a href="https://twitter.com/Vaibhav32756208" ><i>Vaibhav Gupta</i></a></center></p></div>
+            <footer>
+            <div class="container">
+                <center>
+                    <p>Copyright &copy; Vaibhav Gupta. All Rights Reserved  |  Contact Us: +91 91408 81565</p>	
+                </center>
+            </div>
+            </footer>
+        </div>
+</body>
+</html>
+
